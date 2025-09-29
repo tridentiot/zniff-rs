69f4febb9826af38f52464dd4709320407494ed5
Christian Salmony Olsen <christian.salmony@tridentiot.com>
2025-09-29 22:05:53 +0200

    More Serial com

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 use std::io::{self, Write, Read};
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Parser)]
 #[command(name = "toolbox")]
@@ -41,11 +41,31 @@ enum Commands {
         /// Serial port
         #[arg(long)]
         port: String,
+
+        /// Z-Wave region
+        #[arg(long, value_enum)]
+        region: Region,
     },
 }
 
-fn run(port_name: String) {
-    let baud_rate = 115200;
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Region {
+    EU = 0,
+    US = 1,
+    ANZ = 2,
+    HK = 3,
+    IN = 5,
+    IL = 6,
+    RU = 7,
+    CN = 8,
+    USLR = 9,
+    EULR = 11,
+    JP = 32,
+    KR = 33,
+}
+
+fn run(port_name: String, _region: &Region) {
+    let baud_rate = 230_400;
 
     println!("Connecting to {}", port_name);
     let mut port = serialport::new(port_name, baud_rate)
@@ -53,8 +73,16 @@ fn run(port_name: String) {
         .open()
         .expect("Failed to open port");
 
+    /*
+    let get_version: Vec<u8> = vec![
+        0x23, // Command SOF
+        0x03, // Command: 0x01 = Version
+        0x00, // Length
+    ];
+    */
+
     let msg: Vec<u8> = vec![
-        b'#', // SOF
+        0x23, // SOF
         0x01, // Command: 0x01 = Version
         0x00, // Length
     ];
@@ -65,20 +93,31 @@ fn run(port_name: String) {
         Err(e) => eprintln!("Write failed: {}", e),
     }
 
-    let mut buffer: Vec<u8> = vec![0; 32];
-    match port.read(buffer.as_mut_slice()) {
-        Ok(bytes_read) => {
-            println!(
-                "Received ({} bytes): {:?}",
-                bytes_read,
-                &buffer[..bytes_read]
-            );
-        },
-        Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
-            println!("Timed out waiting for response");
-        }
-        Err(e) => {
-            eprintln!("Error reading from serial port: {:?}", e);
+    let mut buffer: Vec<u8> = vec![0; 128];
+    loop {
+        match port.read(buffer.as_mut_slice()) {
+            Ok(bytes_read) => {
+                println!("Received {:?} bytes", bytes_read);
+                for byte in &buffer[..bytes_read] {
+                    print!("0x{:02X} ", byte);
+                }
+                println!();
+
+                /*
+                println!(
+                    "Received ({} bytes): {:x}",
+                    bytes_read,
+                    &buffer[..bytes_read]
+                );
+                */
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => {
+                println!("Timed out waiting for response");
+            }
+            Err(e) => {
+                eprintln!("Error reading from serial port: {:?}", e);
+                break;
+            }
         }
     }
 }
@@ -91,10 +130,11 @@ fn main() {
             println!("Converting '{}' to '{}' as format '{}'", input, output, format);
             // Add conversion logic here
         }
-        Commands::Run { config, debug , port} => {
+        Commands::Run { config, debug , port, region} => {
             println!("Running with config: {:?}", config);
             println!("Debug mode: {}", debug);
-            run(port.to_string());
+            println!("Region: {:?}", region);
+            run(port.to_string(), region);
         }
     }
 }
