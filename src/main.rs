1d1e5c142bdb8ebd8580a9a3635f70fa264a6891
Christian Salmony Olsen <christian.salmony@tridentiot.com>
2026-01-24 18:36:22 +0100

    Made --region required for the run command

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ enum Commands {
         port: String,
 
         /// Z-Wave region
-        #[arg(long, value_enum)]
+        #[arg(long, value_enum, required = true)]
         region: Region,
     },
 
